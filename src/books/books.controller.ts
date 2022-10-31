3d4b7c9ada9e342ca4ae502269a98766fd4b17bb
junior <abelsouzacosta@gmail.com>
2022-10-30 22:46:38 -0300

    feat(books): create
    add endpoint that allows to create a book resource

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import {
+  ClientProxy,
+  ClientProxyFactory,
+  Transport,
+} from '@nestjs/microservices';
+import { RABBITMQ_URL } from 'src/config';
+import { MessagePatterns } from 'src/constants/enums/message-patterns.enum';
+import { Queues } from 'src/constants/enums/queues.enum';
+import { CreateBookDto } from './domain/dto/create-book.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  private readonly client: ClientProxy;
+
+  constructor() {
+    this.client = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: [RABBITMQ_URL],
+        queue: Queues.LIBRARY,
+      },
+    });
+  }
+
+  @Post()
+  @UsePipes(new ValidationPipe())
+  create(@Body() data: CreateBookDto) {
+    return this.client.emit(MessagePatterns.CREATE_BOOK, data);
+  }
+}
--- src/books/domain/dto/create-book.dto.ts
+++ src/books/domain/dto/create-book.dto.ts
@@ -1,7 +1,29 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
 export class CreateBookDto {
+  @IsString({
+    message: 'title should be a string',
+  })
+  @IsNotEmpty({
+    message: 'title is required',
+  })
   title: string;
 
+  @IsString({
+    message: 'description should be a string',
+  })
+  @IsNotEmpty({
+    message: 'description is required',
+  })
   description: string;
 
+  @IsNumber({
+    allowInfinity: false,
+    allowNaN: false,
+    maxDecimalPlaces: 0,
+  })
+  @IsNotEmpty({
+    message: 'number_of_pages is required',
+  })
   number_of_pages: number;
 }
