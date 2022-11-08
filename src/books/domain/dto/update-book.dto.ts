c2f224eb3326b647c2bdc872dfd1cdabf34817fa
junior <abelsouzacosta@gmail.com>
2022-11-08 05:20:24 -0300

    docs(docs): adding documentation for endpoints

--- src/books/domain/dto/update-book.dto.ts
+++ src/books/domain/dto/update-book.dto.ts
@@ -1,4 +1,36 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBookDto } from './create-book.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 
-export class UpdateBookDto extends PartialType(CreateBookDto) {}
+export class UpdateBookDto {
+  @ApiProperty({
+    description: 'Book title',
+    example: 'The Art of Living',
+  })
+  @IsString({
+    message: 'title should be a string',
+  })
+  @IsOptional()
+  title?: string;
+
+  @ApiProperty({
+    description: 'a short description of the book',
+    example: 'stoic reflections to daily life',
+  })
+  @IsString({
+    message: 'description should be a string',
+  })
+  @IsOptional()
+  description?: string;
+
+  @ApiProperty({
+    description: 'number of pages of the book',
+    example: 1900,
+  })
+  @IsNumber({
+    allowInfinity: false,
+    allowNaN: false,
+    maxDecimalPlaces: 0,
+  })
+  @IsOptional()
+  number_of_pages?: number;
+}
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('lirabry')
+    .setTitle('library')
     .setDescription('library api gateway documentation')
     .setVersion('1.0')
     .build();
