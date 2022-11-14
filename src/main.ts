107b60d9d2f5940d852f3f01cf75db0d54035027
junior <abelsouzacosta@gmail.com>
2022-11-13 22:33:24 -0300

    refactor(exceptions): remove exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,8 +13,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-
-  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(3000);
 }
